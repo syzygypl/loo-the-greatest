cc0c8606e91735bddbbb80ab3d42f8366c0ef572
bsk <bartlomiej.glowala@syzygy.pl>
2017-06-03 16:23:34 +0200

    status

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-app.use( bodyParser.json() );
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+let isLooOccupied = false;
+
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('The Loo is ' + (isLooOccupied ? 'occupied' : 'free for use :)'));
+});
 
 app.post('/loo', function (req, res) {
   console.log(req.body);
-  res.send('POST request');
+  if (req.body.status === true) {
+    isLooOccupied = true;
+  } else if (req.body.status === false) {
+    isLooOccupied = false;
+  }
+  res.send('Loo The Greatest is thankful for that!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Loo The Greatest listening on port 3000!')
+});
